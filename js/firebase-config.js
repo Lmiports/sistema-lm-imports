94f7831d8f9afab2b7503b56e12e70cd7f6d6277
Lmiports <marceloluisbasilio@gmail.com>
2026-02-01 21:31:43 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,16 +1,13 @@
-// --- CONFIGURAÇÃO CLÁSSICA (COMPAT) ---
-// Não use 'import' e nem 'const app ='
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBdh-XiLP1C-5Nc044GJv3500ey6dBLaA8",
+  apiKey: "AIzaSyBdH-XiLP1C-5Nc044GJv3500ey6dBLaA8",
   authDomain: "vertice-gestao.firebaseapp.com",
   projectId: "vertice-gestao",
-  storageBucket: "vertice-gestao.appspot.com",
+  storageBucket: "vertice-gestao.firebasestorage.app",
   messagingSenderId: "901320631312",
   appId: "1:901320631312:web:becd5310e01d026f5b5695"
 };
 
-// INICIALIZA O FIREBASE NO MODO GLOBAL
-// (Isso é o que faz o seu index.html funcionar)
+// --- OBRIGATÓRIO: O MOTOR DE ARRANQUE ---
+// Sem isso, o site não conecta no banco
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
