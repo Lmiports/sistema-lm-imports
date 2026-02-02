f901ac7c448ac62fee29bf1577e6160e91991643
Lmiports <marceloluisbasilio@gmail.com>
2026-02-01 21:11:30 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,16 @@
-// Configuração REAL do Vértice BI Core
+// VERSÃO CORRETA PARA SEU HTML (NÃO USE 'IMPORT')
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBdH-XiLP1C-5Nc044GJv35O0ey6dBlaA8",
+  apiKey: "AIzaSyBdh-XiLP1C-5Nc044GJv3500ey6dBLaA8",
   authDomain: "vertice-gestao.firebaseapp.com",
   projectId: "vertice-gestao",
-  storageBucket: "vertice-gestao.firebasestorage.app",
+  storageBucket: "vertice-gestao.appspot.com",
   messagingSenderId: "901320631312",
   appId: "1:901320631312:web:becd5310e01d026f5b5695"
 };
 
-// Inicializa o Firebase (Modo Compatibilidade)
+// Inicializa o Firebase Globalmente
 firebase.initializeApp(firebaseConfig);
+
+// Opcional: Deixar o banco pronto para uso
+// const db = firebase.firestore();
