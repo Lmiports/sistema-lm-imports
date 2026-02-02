80270de0e4ef57cdc42d64a1bfb1c47d402e41be
Lmiports <marceloluisbasilio@gmail.com>
2026-02-01 21:41:07 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,15 +1,13 @@
-// Configuração do seu Firebase (Baseada na sua imagem image_40ac4c.png)
 const firebaseConfig = {
   apiKey: "AIzaSyBdH-XiLP1C-5Nc044GJv3500ey6dBLaA8",
   authDomain: "vertice-gestao.firebaseapp.com",
   projectId: "vertice-gestao",
-  storageBucket: "vertice-gestao.appspot.com",
+  storageBucket: "vertice-gestao.firebasestorage.app",
   messagingSenderId: "901320631312",
   appId: "1:901320631312:web:becd5310e01d026f5b5695"
 };
 
-// Inicializa o Firebase
+// --- OBRIGATÓRIO: O MOTOR DE ARRANQUE ---
+// Sem isso, o site não conecta no banco
 firebase.initializeApp(firebaseConfig);
-
-// Define a variável global db que o sistema usa
 const db = firebase.firestore();
